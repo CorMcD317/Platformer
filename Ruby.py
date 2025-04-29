09be1dc10ea034a4a8314c1254b1c359af629b9c
CorMcD317 <mcdoucor000@warren.k12.in.us>
2025-04-29 09:20:02 -0400

    Added New Code and Completed Ruby Commit

--- Ruby.py
+++ Ruby.py
@@ -9,20 +9,36 @@ class Ruby(pygame.sprite.Sprite):
         super().__init__()
 
         #Set constant variables
+
+        self.VERTICAL_ACCELERATION = 3
+        self.HORIZONTAL_VELOCITY = 5
+        self.WINDOW_WIDTH = window_width
+        self.WINDOW_HEIGHT = window_height
         #TODO: create a self.VERTICAL_ACCELERATION variable and assign 3 to it
         #TODO: create a self.HORIZONTAL_VELOCITY variable and assign 5 to it.
         #TODO: create a self.WINDOW_WIDTH variable and assign window_width to it
         #TODO: create a self.WINDOW_HEIGHT variable and assign window_height to it.
 
         #Animation frames
+        self.ruby_sprites = []
         #TODO: create a self.ruby_sprites variables and assign an empty list to it.  HINT:  []
 
         #Rotating
         self.ruby_sprites.append(
             pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile000.png"), (64, 64)))
+        pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile001.png"), (64, 64))
+        pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile002.png"), (64, 64))
+        pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile003.png"), (64, 64))
+        pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile004.png"), (64, 64))
+        pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile005.png"), (64, 64))
+        pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile006.png"), (64, 64))
         #TODO: so we've just added an image to our list of ruby_sprites.  repeate the previous line form tile's 001 to 006
 
         #Load image and get rect
+        self.current_sprite = 0
+        self.image = self.ruby_sprites[self.current_sprite]
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft(window_width // 2, 100)
         #TODO: create a self.current_sprite variable and assign 0 to it
         #TODO: create a self.image variable and assign self.ruby_sprites[self.current_sprite] to it
         #TODO: create a self.rect variable and assign self.image.get_rect() to it.
@@ -33,6 +49,7 @@ class Ruby(pygame.sprite.Sprite):
         self.portal_group = portal_group
 
         #Load sounds
+        self.portal_sound = pygame.mixer.Sound("assets/sounds/portal_sound.wav")
         #TODO: create a self.portal_sound and assign pygame.mixer.Sound() to it.  pass in "assets/sounds/portal_sound.wav"
 
         #Kinematic vectors
@@ -42,6 +59,9 @@ class Ruby(pygame.sprite.Sprite):
 
     def update(self):
         """Update the ruby"""
+        self.animate(self.ruby_sprites, 0.25)
+        self.move()
+        self.check_collisions()
         #TODO: call self.animate() passing in self.ruby_sprites and 0.25 into the function
         #TODO: call self.move()
         #TODO: call self.check_collisions()
@@ -51,6 +71,8 @@ class Ruby(pygame.sprite.Sprite):
         #We don't need to update the acceleration vector because it never changes here
 
         # Calculate new kinematics values: (4, 1) + (2, 8) = (6, 9)
+        self.acceleration += self.velocity
+        self.velocity + 0.5 * self.acceleration
         #TODO: add self.acceleration to self.velocity.  HINT: when I say add y to x I mean x += y or x = x + y
         #TODO: add self.velocity + 0.5 * self.acceleration to self.position
 
@@ -59,6 +81,8 @@ class Ruby(pygame.sprite.Sprite):
             self.position.x = self.WINDOW_WIDTH
         elif self.position.x > self.WINDOW_WIDTH:
             self.position.x = 0
+
+        self.position = self.rect.bottom
 
         #TODO: assign self.position to self.rect.bottomleft.  HINT:  When I say assign y to x I mean x = y
 
@@ -89,7 +113,12 @@ class Ruby(pygame.sprite.Sprite):
 
     def animate(self, sprite_list, speed):
         """Animate the ruby"""
+        if self.current_sprite < len(sprite_list) - 1:
+            speed += self.current_sprite
+        else : self.current_sprite = 0
         #TODO: check if self.current_sprite is less than len(sprite_list) - 1.  If so add speed to self.current_sprite
         #TODO: else assign 0 to self.current_sprite
+
+        sprite_list[int(self.current_sprite)] = self.image
 
         #TODO: assign sprite_list[int(self.current_sprite)] to self.image.  When I say assign y to x I mean x = y
